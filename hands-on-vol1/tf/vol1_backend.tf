7704d87992b8ae37379c391c5b405439ab2b1047
竹本将気 <t.masaki0618@gmail.com>
2017-07-09 19:03:24 +0900

    戻し

--- hands-on-vol1/tf/vol1_backend.tf
+++ hands-on-vol1/tf/vol1_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hands-on-terraform-state2"
+    bucket = "hands-on-terraform-state"
     region = "ap-northeast-1"
     key    = "terraform.tfstate"
     #lock_table = "terraform-state-lock"
