e0e19693920314ae6f8b2c252656b9f5b3dc41e0
竹本将気 <t.masaki0618@gmail.com>
2017-07-09 21:51:03 +0900

    wordpressのセットアップ

--- hands-on-vol1/tf/vol1_web_userdata.sh
+++ hands-on-vol1/tf/vol1_web_userdata.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
 cd /tmp
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
+# パッケージインストール
+sudo yum install -y httpd php php-devel php-mysql php-mbstring php-gd
+
+# apacheの設定変更
+sudo sed -i -e 's/ServerTokens OS/ServerTokens Prod/' /etc/httpd/conf/httpd.conf
+sudo sed -i -e 's/ServerSignature On/ServerSignature Off/' /etc/httpd/conf/httpd.conf
+sudo sed -i -e 's/Options FollowSymLinks/Options -Indexes FollowSymLinks/' /etc/httpd/conf/httpd.conf
+
+# phpの設定変更
+sudo sed -i -e 's/;mbstring.language/mbstring.language/' /etc/php.ini
+sudo sed -i -e 's/;mbstring.internal_encoding = EUC-JP/mbstring.internal_encoding = UTF-8/' /etc/php.ini
+sudo sed -i -e 's/;mbstring.http_input/mbstring.http_input/' /etc/php.ini
+sudo sed -i -e 's/;mbstring.detect_order/mbstring.detect_order/' /etc/php.ini
+sudo sed -i -e 's/;date.timezone =/date.timezone = Asia\/Tokyo/' /etc/php.ini
+
+# Apahceの設定チェック
+sudo service httpd configtest
+
+# Apacheの自動起動設定
+sudo chkconfig httpd on
+
+# wordpressをダウンロードして、展開
+cd /var/www/html
+sudo wget http://ja.wordpress.org/latest-ja.tar.gz
+sudo tar zxvf latest-ja.tar.gz
+
+
+# ディレクトリ名をwpに変更し、ディレクトリのオーナーをapacheに変更
+sudo mv wordpress wp
+sudo chown -R apache:apache wp
+cd wp
+sudo cp -p wp-config-sample.php wp-config.php
+
+# wordpressの設定変更
+sudo sed -i -e 's/database_name_here/wordpress/' wp-config.php
+sudo sed -i -e 's/username_here/wordpress/' wp-config.php
+sudo sed -i -e 's/password_here/password/' wp-config.php
+sudo sed -i -e 's/localhost/wordpress.cic5wtmdlea0.ap-northeast-1.rds.amazonaws.com/' wp-config.php
+
+# Apacheの再起動
+sudo service httpd restart
